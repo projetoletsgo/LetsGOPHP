88400322a0a65b236dff53316e617663c62b88d9
halineferreira <halineferreira@gmail.com>
2016-11-26 17:12:09 -0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include ('/view/includes/_header.php'); ?>
+test
+<?php include ('/view/includes/_footer.php'); ?>
+		
---
+++ view/includes/_menu.php
@@ -0,0 +1,10 @@
+<a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+<div id="slide-out" class="side-nav fixed">
+    <div class="sidenav-logo">
+        <img class="logo" src="assets/images/logo.png">
+    </div>
+    <ul>
+        <li><a title="Minhas Viagens" href="MinhasViagens">Minhas Viagens</a></li>
+        <li><a title="Cadastrar Excursão" href="CadastrarExcursao">Cadastrar Excursão</a></li>
+    </ul>
+</div>
